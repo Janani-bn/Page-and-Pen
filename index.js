17e58009936d73b72e327ac2f1ecce4277108d76
Janani-bn <hemapriyan@Jananis-MacBookAir.local>
2026-05-14 11:34:13 +0530

    add pooler for database connection

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const db = new pg.Client({
     database : process.env.DB_NAME,
     password : process.env.DB_PASSWORD,
     port : process.env.DB_PORT,
+});
+const pool = new Pool({
     connectionString: process.env.DB_URL,
     ssl: { rejectUnauthorized: false }
 });
